ac3b07fe88aa276e8fe9e6c3aa9eba96278b3be7
iamthenoname <iamthenoname@proton.me>
2025-02-22 16:26:10 +0800

    nix: fix formatting

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,37 +1,40 @@
-{ pkgs, perSystem, }:
+{ pkgs, perSystem }:
 pkgs.mkShell {
-          packages = [
-            pkgs.colcon
+  packages = [
+    pkgs.colcon
 
-            pkgs.python3Packages.pyserial
-            pkgs.python3Packages.python-can
-            pkgs.python3Packages.python-lsp-server
+    pkgs.python3Packages.pyserial
+    pkgs.python3Packages.python-can
+    pkgs.python3Packages.python-lsp-server
 
-            pkgs.just
-            pkgs.nil
-            pkgs.nixd
+    pkgs.just
+    pkgs.nil
+    pkgs.nixd
 
-            perSystem.self.ydlidar-sdk
-            # ... other non-ROS packages
-            (with pkgs.rosPackages.jazzy; buildEnv {
-              paths = [
-                ros-core
-                ament-cmake-core
+    perSystem.self.ydlidar-sdk
+    # ... other non-ROS packages
+    (
+      with pkgs.rosPackages.jazzy;
+      buildEnv {
+        paths = [
+          ros-core
+          ament-cmake-core
 
-                rclcpp
+          rclcpp
 
-                teleop-twist-joy
-                teleop-twist-keyboard
-                joy
+          teleop-twist-joy
+          teleop-twist-keyboard
+          joy
 
-                twist-stamper
+          twist-stamper
 
-                tf2-ros
-                tf2-ros-py
-                ros2-control
-                diff-drive-controller
-                # ... other ROS packages
-              ];
-            })
-          ];
-        }
+          tf2-ros
+          tf2-ros-py
+          ros2-control
+          diff-drive-controller
+          # ... other ROS packages
+        ];
+      }
+    )
+  ];
+}
--- nix/packages/ydlidar-sdk.nix
+++ nix/packages/ydlidar-sdk.nix
@@ -1,23 +1,19 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation {
-          pname = "ydlidar-sdk";
-          version = "1.2.7";
-          src = pkgs.fetchFromGitHub {
-            owner = "YDLIDAR";
-            repo = "YDLidar-SDK";
-            rev = "V1.2.7";
-            hash = "sha256-9F6pX/1CdzrXnkQ3yiPKTZERtQAXKXzfjclhhQgsn+4=";
-          };
+  pname = "ydlidar-sdk";
+  version = "1.2.7";
+  src = pkgs.fetchFromGitHub {
+    owner = "YDLIDAR";
+    repo = "YDLidar-SDK";
+    rev = "V1.2.7";
+    hash = "sha256-9F6pX/1CdzrXnkQ3yiPKTZERtQAXKXzfjclhhQgsn+4=";
+  };
 
-          buildInputs = [
-            pkgs.cmake
-          ];
+  buildInputs = [ pkgs.cmake ];
 
-          hardeningDisable = [
-            "format"
-          ];
+  hardeningDisable = [ "format" ];
 
-          configurePhase = ''
-            cmake -DCMAKE_INSTALL_PREFIX=$out .
-          '';
-        }
+  configurePhase = ''
+    cmake -DCMAKE_INSTALL_PREFIX=$out .
+  '';
+}
